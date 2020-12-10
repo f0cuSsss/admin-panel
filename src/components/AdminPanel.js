50aca63c79c234ef8257b47bc9ade74c872b5a29
f0cuS <f0cuSmcsza@gmail.com>
2020-12-10 11:48:18 +0200

    Initial template

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AdminPanel extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Admin panel
+            </div>
+        );
+    }
+}
+
+export default AdminPanel;
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+import AdminPanel from './AdminPanel'
+
+export default () => {
+    return (
+        <div>
+            <AdminPanel />
+        </div>
+    );
+}
