747c36fe3e2bfbe2ac8022a43e83747272dfaf29
f0cuS <f0cuSmcsza@gmail.com>
2020-12-11 19:26:48 +0200

    Header bar design created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import '../styles/Header.css'
+
 export const Header = () => {
     return (
         <header>
+            <span className="header-text center">Admin panel</span>
+
+            <div className="detail center">
+                <i className="users icon small"/>
+                <span>1248</span>
+            </div>
+
+            <div className="detail center">
+                <i className="eye icon small"/>
+                <span>872</span>
+            </div>
+
+            <div className="signin-icon center">
+                <i className="sign-out icon large"/>
+            </div>
 
         </header>
     );
