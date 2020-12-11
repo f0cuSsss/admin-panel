47bc2fbd9eebcadfeb7717d4c2cd94b1f4d9a129
f0cuS <f0cuSmcsza@gmail.com>
2020-12-11 16:23:16 +0200

    Navigation design created

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,56 @@
 import React from 'react'
 
-import '../styles/AdminPanel.css'
+import '../styles/Navigation.css'
+import logo from '../images/logo.png'
+
 
 export const Navigation = () => {
     return (
-        <aside className="navigation-menu">
-            Navigation
+        <aside class="nav-grid">
+
+            <img alt="" src={logo}/>
+
+            <div>
+                <div class="item nav-item" >
+                    <i className="chart bar icon large" />
+                    <div 
+                        class="middle aligned content" >
+                            Activity
+                    </div>
+                </div>
+
+                <div class="item nav-item" >
+                    <i className="list icon large" />
+                    <div 
+                        class="middle aligned content" >
+                            Ban-list
+                    </div>
+                </div>
+
+                <div class="item nav-item" >
+                    <i className="tasks icon large" />
+                    <div 
+                        class="middle aligned content" >
+                            Wishes
+                    </div>
+                </div>
+
+                <div class="item nav-item" >
+                    <i className="paste icon large" />
+                    <div 
+                        class="middle aligned content" >
+                            Reports
+                    </div>
+                </div>
+
+                <div class="item nav-item" >
+                    <i className="archive icon large" />
+                    <div 
+                        class="middle aligned content" >
+                            Archive
+                    </div>
+                </div>
+            </div>
         </aside>
     );
 }
