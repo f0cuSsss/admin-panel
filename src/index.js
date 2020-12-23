36da851dcc95ec7abb894c5ad0c1171ada324d8c
f0cuS <f0cuSmcsza@gmail.com>
2020-12-23 13:11:42 +0200

    Import error fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
-import reducers from '../reducers'
+import reducers from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
