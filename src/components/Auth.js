779067d9ac9814d0e9dc5e5d07b5550b2afafa9b
f0cuS <f0cuSmcsza@gmail.com>
2020-12-23 16:46:28 +0200

    Auth with Redux

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,33 +1,63 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import '../styles/Auth.css'
 import logo from '../images/logo.png'
+import { signIn } from '../actions'
 
 class Auth extends React.Component {
 
+    onSignInClick() {
+
+    }
+    
     render() {
-        return (
-            <div className="auth">
-                <div className="auth-block">
-                    <img alt="" src={logo}/>
-                    <div className="field-block">
-                        <div class="ui input">
-                            <input placeholder="Login" />
-                        </div>
-                        <div class="ui input">
-                            <input placeholder="Password" />
+        
+        if(this.props.isSignedIn){
+            return (
+                <div className="auth">
+                    <div className="auth-block">
+                        <img alt="" src={logo}/>
+                        <h2>You signed in</h2>
+                        <div className="button">
+                            <Link to="/dashboard/">
+                                <div className="singedin-nav">
+                                    Dashboard
+                                </div>
+                            </Link>
                         </div>
                     </div>
-
-                    <div className="error-message"></div>
-
-                    <div className="button">Sign in</div>
                 </div>
-            </div>
-        );
+            );
+        }
+        else{
+            return (
+                <div className="auth">
+                    <div className="auth-block">
+                        <img alt="" src={logo}/>
+                        <div className="field-block">
+                            <div className="ui input">
+                                <input placeholder="Login" />
+                            </div>
+                            <div className="ui input">
+                                <input placeholder="Password" />
+                            </div>
+                        </div>
+                        <div className="error-message"></div>
+                        <div className="button" onClick={() => this.props.signIn()}>Sign in</div>
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
-export default Auth;
+const mapStateToProps = state => {
+    console.log(state);
+    return { isSignedIn: state.auth.isSignedIn }
+}
+
+export default connect(mapStateToProps, { signIn })(Auth);
 
 
